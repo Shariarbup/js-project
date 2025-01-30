39662dbf13bff9ee7daf5f134690ea777770983a
Al Shariar <35493040+Shariarbup@users.noreply.github.com>
2025-01-30 12:49:23 +0600

    added toast-message

---
+++ message-toaster/main.js
@@ -0,0 +1,56 @@
+window.onload = () => {
+    main();
+}
+
+let div = null;
+function main() {
+    const root = document.getElementById('root');
+    const button = document.getElementById('change-button');
+    const output = document.getElementById('output');
+    const copyBtn = document.getElementById('copy-btn');
+
+    button.addEventListener('click', function () {
+        const hexColor = genrateHexColor();
+        root.style.backgroundColor = hexColor;
+        output.value = hexColor;
+
+    })
+
+
+    copyBtn.addEventListener('click', function (){
+        navigator.clipboard.writeText(output.value); 
+        if (div !== null) {
+            div.remove();
+            div = null;
+        }
+        generateMessageToaster(`${output.value} copied`);
+    })
+}
+
+function genrateHexColor() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+
+}
+
+function generateMessageToaster(msg) {
+    console.log("i am from message toaster")
+    div = document.createElement('div');
+    
+    div.innerText = msg;
+    div.className = 'toast-message toast-message-slide-in';
+    div.addEventListener('click', function() {
+        div.classList.remove('toast-message-slide-in');
+        div.classList.add('toast-message-slide-out');
+        div.addEventListener('animationend', function() {
+            div.remove();
+            div = null;
+        })
+    })
+
+    
+    document.body.appendChild(div);
+}
